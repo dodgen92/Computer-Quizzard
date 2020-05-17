1f8e9655ad5701ef76eb5e9283e35ca4f6736e87
dodgen92 <58264866+dodgen92@users.noreply.github.com>
2020-05-17 02:26:33 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Home from './components/Home';
 import Play from './components/quiz/Play';
 import QuizSummary from './components/quiz/QuizSummary';
-function App() {
+
+ function App() {
   return (
     <Router>
       <Route path="/" exact component={Home} />
