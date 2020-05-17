96770c0452d1b10cee5b8279c13ff45aee170e54
dodgen92 <58264866+dodgen92@users.noreply.github.com>
2020-05-17 02:03:29 -0400

    deleted register component route for now

--- src/App.js
+++ src/App.js
@@ -3,12 +3,10 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Home from './components/Home';
 import Play from './components/quiz/Play';
 import QuizSummary from './components/quiz/QuizSummary';
-import Register from './components/Register/Register'
 function App() {
   return (
     <Router>
       <Route path="/" exact component={Home} />
-      <Route path="/Register" exact component={Register} />
       <Route path="/play/quiz" exact component={Play} />
       <Route path="/play/quizSummary" exact component={QuizSummary} />
     </Router>
